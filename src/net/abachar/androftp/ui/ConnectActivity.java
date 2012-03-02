c7ad31385c804e959cf09d8018d1adaef97d3370
buchir@gmail.com <buchir@gmail.com@45a50be9-4c5a-4879-ab2e-1cd9afa1f828>
2012-03-02 17:04:41 +0000

    Create initial project

---
+++ src/net/abachar/androftp/ui/ConnectActivity.java
@@ -0,0 +1,18 @@
+package net.abachar.androftp.ui;
+
+import net.abachar.androftp.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author abachar
+ */
+public class ConnectActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
